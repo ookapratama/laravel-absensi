cfb3342312dec4dd03f553c8ef70f1531262486e
ookapratama <ookapratama2@gmail.com>
2026-02-08 20:12:08 +0800

    chore : add log in informasi feature

--- app/Models/Informasi.php
+++ app/Models/Informasi.php
@@ -4,14 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Traits\LogsActivity;
+
 class Informasi extends Model
 {
+    use LogsActivity;
+
     protected $fillable = [
         'user_id',
         'gambar',
         'judul',
         'isi',
     ];
+
+    /**
+     * Custom description for activity log
+     */
+    protected static function getLogDescription(string $action, Model $model): string
+    {
+        $identifier = $model->getAttribute('judul') ?? $model->getKey();
+        
+        return match ($action) {
+            'created' => "Informasi '{$identifier}' telah diterbitkan",
+            'updated' => "Informasi '{$identifier}' telah diperbarui",
+            'deleted' => "Informasi '{$identifier}' telah dihapus",
+            default => "Informasi '{$identifier}' - {$action}",
+        };
+    }
 
     public function user()
     {
@@ -23,6 +42,6 @@ class Informasi extends Model
         if ($this->gambar) {
             return \Illuminate\Support\Facades\Storage::url($this->gambar);
         }
-        return asset('assets/img/elements/18.jpg'); // Default placeholder
+        return asset('assets/img/elements/18.png'); // Default placeholder
     }
 }
