468e16bdf51047a93dbcc1d0fd3c9313c5e29ea9
ookapratama <ookapratama2@gmail.com>
2026-02-14 11:18:12 +0800

    chore : add guest middleware in login routes

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductsController;
 
 // Auth Routes
-Route::get('login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('login', [AuthController::class, 'login']);
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('login', [AuthController::class, 'login']);
+});
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
